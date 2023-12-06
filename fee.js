75dcf1736cad6cd046e551100a0a506eb2b91ef7
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-06 23:15:47 +0700

    Add files via upload

--- fee.js
+++ fee.js
@@ -5,6 +5,12 @@ function calculateLandFee(nextInputId) {
     var length = parseFloat(document.getElementById("length").value);
     var volume = (width * height * length) / 6000;
 
+    // Check if either volume or weight is equal to or over 100,000
+    if (volume >= 100000 || weight >= 100000) {
+        document.getElementById("notify").innerText = "សូមបញ្ជាក់: ទំនិញនេះជាប្រភេទម៉ែត្រគីប!";
+        return; // Stop further calculations
+    }
+
     var fee, fee1, fly, fly1, notification;
 
     if (!isNaN(weight) && weight > 0 && weight >= volume) {
@@ -14,13 +20,20 @@ function calculateLandFee(nextInputId) {
         fly = weight * 7;
         fly1 = weight * 8;
         notification = "Weight: " + weight.toFixed(2) + " KG";
-    } else {
+    } else if (!isNaN(volume) && volume > 0 && volume > weight) {
         // Calculate fee based on volume
         fee = volume * 1.5;
         fee1 = volume * 2;
         fly1 = volume * 8;
         fly = volume * 7;
-        notification = "Volume: " + volume + " KG";
+        notification = "Volume: " + volume.toFixed(8) + " KG";
+    } else {
+        // If weight is not greater than or equal to volume, set fees to zero
+        fee = 0;
+        fee1 = 0;
+        fly1 = 0;
+        fly = 0;
+        notification = "Invalid input. Please check your values.";
     }
 
     // Display both fees
@@ -28,7 +41,7 @@ function calculateLandFee(nextInputId) {
     document.getElementById("fee1").innerText = "USD " + fee1.toFixed(2);
 
     // Display volume
-    document.getElementById("volume").innerText = volume + " KG";
+    document.getElementById("volume").innerText = volume.toFixed(8) + " KG";
 
     // Display notification
     document.getElementById("notify").innerText = "Calculation based on " + notification;
