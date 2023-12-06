4238c83e756b56ccc260acf4b953938f825562cc
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-06 13:24:53 +0700

    Add files via upload

--- fee.js
+++ fee.js
@@ -1,10 +1,9 @@
 function calculateLandFee() {
     var weight = parseFloat(document.getElementById("weight").value);
-    var w = parseFloat(document.getElementById("width").value);
-    var h = parseFloat(document.getElementById("height").value);
-    var l = parseFloat(document.getElementById("length").value);
-
-    var volume = (w * h * l) / 6000;
+    var width = parseFloat(document.getElementById("width").value);
+    var height = parseFloat(document.getElementById("height").value);
+    var length = parseFloat(document.getElementById("length").value);
+    var volume = (width * height * length) / 6000;
 
     var fee, fee1, fly, fly1, notification;
 
@@ -12,35 +11,36 @@ function calculateLandFee() {
         // Calculate fee based on weight
         fee = weight * 1.5;
         fee1 = weight * 2;
-        fly = weight * 7;  // Corrected calculation for fly
-        fly1 = weight * 8; // Corrected calculation for fly1
-        notification = "Weight: " + weight.toFixed(2) + " KG";
+        fly = weight * 7;
+        fly1 = weight * 8;
+        notification = "Weight: " + weight + " KG";
     } else {
         // Calculate fee based on volume
         fee = volume * 1.5;
         fee1 = volume * 2;
         fly1 = volume * 8;
-        fly - volume * 7;
-        notification = "Volume: " + volume.toFixed(2) + " KG";
+        fly = volume * 7;
+        notification = "Volume: " + volume + " KG";
     }
 
     // Display both fees
-    document.getElementById("fee").value = "USD " + fee.toFixed(2);
-    document.getElementById("fee1").value = "USD " + fee1.toFixed(2);
+    document.getElementById("fee").innerText = "USD " + fee;
+    document.getElementById("fee1").innerText = "USD " + fee1;
 
     // Display volume
-    document.getElementById("volume").innerText = volume.toFixed(2) + " KG";
+    document.getElementById("volume").innerText = volume + " KG";
 
     // Display notification
     document.getElementById("notify").innerText = "Calculation based on " + notification;
 
     // Display additional fees
-    document.getElementById("fly").value = "USD " + (fly).toFixed(2);
-    document.getElementById("fly1").value = "USD " + (fly1).toFixed(2);
+    document.getElementById("fly").innerText = "USD " + fly;
+    document.getElementById("fly1").innerText = "USD " + fly1;
 }
 
-document.getElementById("length").addEventListener("keyup", function(event) {
+document.getElementById("length").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
+        event.preventDefault(); // Prevent the default form submission
         calculateLandFee();
         document.getElementById("order").focus();
     }
