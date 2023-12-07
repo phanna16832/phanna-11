2a4dbbfc5a8e9bf34461fd6dd7549c5108c2ec4a
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-07 05:31:51 +0000

    update1.03

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
       }
-    }
+
+      // Close Dropdown1 if the user clicks outside of it
+      var dropdown1 = document.getElementById("myDropdown");
+      if (dropdown1.classList.contains('show') && !event.target.matches('.dropbutton')) {
+          dropdown1.classList.remove('show');
+      }
   }
 }
