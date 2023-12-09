0886494baa887894ebc28e5ac2c1be48c49a80e0
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-09 10:23:30 +0000

    update

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 function myFunction() {
     var dropdownContent = document.getElementById("myDropdown");
     dropdownContent.classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
     if (!event.target.matches('.drop')) {
-        var dropdowns = document.querySelectorAll('.dropdown-content1');
-        dropdowns.forEach(function(openDropdown) {
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        });
+      var dropdowns = document.querySelectorAll('.dropdown-content1');
+      dropdowns.forEach(function(openDropdown) {
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      });
     }
-};
+  };
+  
