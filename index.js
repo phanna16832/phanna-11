df7130aaca32eafa7e681c77259ab0df2287b3c7
Phanna16832 <your.email@example.com>
2023-12-12 22:00:34 +0700

    update

--- index.js
+++ index.js
@@ -22,4 +22,36 @@ function toggleDropdown(dropdownId) {
       hideAllDropdowns();
     }
   }
-  
+/*search bar js code*/
+function performSearch() {
+  // Get the input value from the search bar
+  var searchInput = document.getElementById('searchBar').value.toLowerCase();
+
+  // Perform your search logic here
+  var data = [
+    { titles: ["telegram", "纸飞机"], file: "telegramSave reply.html" },
+    { titles: ["ccsl"], file: "ccsl.html" },
+    { titles: ["fee", "运费"], file: "calculate.html" },
+    // Add more results as needed
+  ];
+
+  // Filter data based on the search input
+  var filteredResults = data.filter(result => result.titles.some(title => title.toLowerCase().includes(searchInput)));
+
+  // Display the results in the searchResults div
+  displayResults(filteredResults);
+}
+
+function displayResults(results) {
+  var searchResultsContainer = document.getElementById('searchResults');
+  // Clear previous results
+  searchResultsContainer.innerHTML = '';
+
+  // Display each result with content loaded from an external file
+  results.forEach(result => {
+    var resultItem = document.createElement('div');
+    // Display all titles for the result
+    resultItem.innerHTML = `<h3>${result.titles.join(' / ')}</h3><a href="${result.file}">View Details</a>`;
+    searchResultsContainer.appendChild(resultItem);
+  });
+}
