3fe5c52c3558263773c15181e79dbe50f3cf54cb
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-17 04:56:37 +0000

    update

--- project1.js
+++ project1.js
@@ -1,66 +1,69 @@
-//copy text//
+// Copy text function
 function copyText(elementId) {
-    var preText = document.getElementById(elementId);
-    var textToCopy = preText.innerText;
-  
-    // Create a temporary textarea element
-    var tempTextarea = document.createElement('textarea');
-    tempTextarea.value = textToCopy;
-    document.body.appendChild(tempTextarea);
-  
-    // Select the text in the textarea
-    tempTextarea.select();
-    tempTextarea.setSelectionRange(0, 99999); // For mobile devices
-  
-    try {
-      // Attempt to copy the selected text to the clipboard
+  var preText = document.getElementById(elementId);
+  var textToCopy = preText.innerText;
+
+  var tempTextarea = document.createElement('textarea');
+  tempTextarea.value = textToCopy;
+  document.body.appendChild(tempTextarea);
+
+  tempTextarea.select();
+  tempTextarea.setSelectionRange(0, 99999); // For mobile devices
+
+  try {
       var successful = document.execCommand('copy');
       var message = successful ? 'Text copied to clipboard' : 'Unable to copy text';
       console.log(message);
-    } catch (err) {
+  } catch (err) {
       console.error('Error copying text', err);
-    } finally {
-      // Remove the temporary textarea
+  } finally {
       document.body.removeChild(tempTextarea);
-    }
   }
-  // Search button
+}
+
+// Search button function
 function performSearch() {
-    // Get the input value from the search bar
-    var searchInput = document.getElementById('searchBar').value.toLowerCase();
-  
-    // Perform your search logic here
-    var data = [
+  var searchInput = document.getElementById('searchBar').value.toLowerCase();
+
+  var data = [
       { titles: ["telegram", "纸飞机"], file: "telegramSave reply.html" },
       { titles: ["ccsl"], file: "ccsl.html" },
       { titles: ["fee", "运费"], file: "calculate.html" },
       { titles: ["taobao", "pinduoduo"], file: "project1.html" },
-      // Add more results as needed
-    ];
-  
-    // Filter data based on the search input
-    var filteredResults = data.filter(result => 
-      result.titles.some(title => title.toLowerCase().includes(searchInput))
-    );
-  
-    // Display the results in the searchResults div
-    displayResults(filteredResults);
-  }
-  
-  // Display search results
-  function displayResults(results) {
-    var searchResultsContainer = document.getElementById('searchResults');
-    
-    // Clear previous results
-    searchResultsContainer.innerHTML = '';
-  
-    // Display each result with content loaded from an external file
-    results.forEach(result => {
-      var resultItem = document.createElement('div');
+      { titles: ["comment", "facebook"], content: `
+        សួស្តីបង 
+        ពត៌មានរបៀបប្រើសេវាជួយទិញទំនិញដោយឥតគិតថ្លៃ : https://shorturl.at/pqQW6
+        Join Telegram Group ជួយទិញ: https://t.me/+CyEdYOtceZkwZjQ1
+        បងសូមចុចចុះឈ្មោះតាមលីងនេះសិនមកបង
+        https://m.ceccsl.com/reg/khmindex?tj
+        Telegram ក្រុមការងារបញ្ជាទិញ : https://t.me/CE_EXPRESS
       
-      // Display all titles for the result
-      resultItem.innerHTML = `<h3>${result.titles.join(' ')}</h3><a href="${result.file}">View Details</a>`;
+        *សំរាប់ការការបង្រៀនទិញបងអាចទាក់ទងតេឡេក្រាមខាងក្រោមបាន 
+        https://t.me/+zaVpyk9lVlI2YmU1
+        ជួយទិញ និង បង្រៀនទិញទំនិញពីប្រទេសចិនដោយឥតគិតថ្លៃ ជាមួយក្រុមហ៊ុនស៊ីអ៊ី` },
+      // Add more results as needed
+  ];
+
+  var filteredResults = data.filter(result => result.titles.some(title => title.toLowerCase().includes(searchInput)));
+
+  displayResults(filteredResults);
+}
+
+// Display search results function
+function displayResults(results) {
+  var searchResultsContainer = document.getElementById('searchResults');
+  searchResultsContainer.innerHTML = '';
+
+  results.forEach(result => {
+      var resultItem = document.createElement('div');
+      var additionalText = result.content || "Additional information not available.";
+
+      if (result.file) {
+          resultItem.innerHTML = `<h3>${result.titles.join('/')}</h3><p>${additionalText}</p><a href="${result.file}" target="_blank">View Details</a>`;
+      } else {
+          resultItem.innerHTML = `<h3>${result.titles.join('/ ')}</h3><p>${additionalText}</p>`;
+      }
+
       searchResultsContainer.appendChild(resultItem);
-    });
-  }
-  
+  });
+}
