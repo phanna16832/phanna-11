f571eb0bf7d2837e062293b9b4803db38558250c
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-17 03:08:05 +0000

    update

--- telegram.js
+++ telegram.js
@@ -1,3 +1,4 @@
+//copy text button//
 function copyText(elementId) {
   var textElement = document.getElementById(elementId);
 
@@ -16,4 +17,38 @@ function copyText(elementId) {
 
   // Remove the temporary input element
   document.body.removeChild(tempInput);
+}
+//search bar//
+function performSearch() {
+  // Get the input value from the search bar
+  var searchInput = document.getElementById('searchBar').value.toLowerCase();
+
+  // Perform your search logic here
+  var data = [
+    { titles: ["telegram", "纸飞机"], file: "telegramSave reply.html" },
+    { titles: ["ccsl"], file: "ccsl.html" },
+    { titles: ["fee", "运费"], file: "calculate.html" },
+    { titles: ["taobao", "pinduoduo"], file: "project1.html" },
+    // Add more results as needed
+  ];
+
+  // Filter data based on the search input
+  var filteredResults = data.filter(result => result.titles.some(title => title.toLowerCase().includes(searchInput)));
+
+  // Display the results in the searchResults div
+  displayResults(filteredResults);
+}
+
+function displayResults(results) {
+  var searchResultsContainer = document.getElementById('searchResults');
+  // Clear previous results
+  searchResultsContainer.innerHTML = '';
+
+  // Display each result with content loaded from an external file
+  results.forEach(result => {
+    var resultItem = document.createElement('div');
+    // Display all titles for the result
+    resultItem.innerHTML = `<h3>${result.titles.join(' / ')}</h3><a href="${result.file}">View Details</a>`;
+    searchResultsContainer.appendChild(resultItem);
+  });
 }
