b2b198c6f0df28fa18f03ecf393724d43289b466
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-18 23:14:02 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,85 +1,99 @@
-function toggleDropdown(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
-    if (dropdown.style.display === "block") {
-      dropdown.style.display = "none";
-    } else {
-      // Hide all other dropdowns before showing the current one
-      hideAllDropdowns();
-      dropdown.style.display = "block";
-    }
-  }
-  
-  function hideAllDropdowns() {
-    var dropdowns = document.querySelectorAll('.dropdown-content');
-    dropdowns.forEach(function (dropdown) {
-      dropdown.style.display = 'none';
-    });
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function (e) {
-    if (!e.target.matches('.dropbtn')) {
-      hideAllDropdowns();
-    }
-  }
-/*search bar js code*/
+document.addEventListener("DOMContentLoaded", function () {
+    var searchButton = document.getElementById('searchButton');
+    searchButton.addEventListener('click', performSearch);
+});
+
 function performSearch() {
-  // Get the input value from the search bar
-  var searchInput = document.getElementById('searchBar').value.toLowerCase();
+    // Get the input value from the search bar
+    var searchInput = document.getElementById('searchBar').value.toLowerCase();
 
-  // Perform your search logic here
-  var data = [
-    { titles: ["telegram", "纸飞机"], file: "telegramSave reply.html" },
-    { titles: ["ccsl"], file: "ccsl.html" },
-    { titles: ["fee", "运费"], file: "calculate.html" },
-    { titles: ["taobao", "pinduoduo"], file: "project1.html" },
-    // Add more results as needed
-  ];
+    // Check if the search input is not empty
+    if (searchInput.trim() !== '') {
+        // Perform your search logic here
+        var data = [
+            { title: "Telegram", file: "telegramSave reply.html" },
+            { title: "CCSL", file: "ccsl.html", img: "ccsl-image.jpg", text: "Description for CCSL" },
+            { title: "Fee Calculation", file: "calculate.html"},
+            { title: "Taobao and Pinduoduo", file: "project1.html", img: "taobao1.jpg"},
+            { title: "id and CE ID", text: "Password:888888 ID:  លេខទូរសព្ទ័: 0 " }
+            // { title: "id and CE ID", file: " ", text: "Password:888888 ID:  លេខទូរសព្ទ័: 0 " }
+            // Add more results as needed
+        ];
 
-  // Filter data based on the search input
-  var filteredResults = data.filter(result => result.titles.some(title => title.toLowerCase().includes(searchInput)));
+        // Filter data based on the search input
+        var filteredResults = data.filter(result => result.title.toLowerCase().includes(searchInput));
 
-  // Display the results in the searchResults div
-  displayResults(filteredResults);
+        // Display the results in the searchResults div
+        displayResults(filteredResults);
+    } else {
+        // Clear the search results if the input is empty
+        clearResults();
+    }
 }
 
 function displayResults(results) {
-  var searchResultsContainer = document.getElementById('searchResults');
-  // Clear previous results
-  searchResultsContainer.innerHTML = '';
+    var searchResultsContainer = document.getElementById('searchResults');
+    // Clear previous results
+    searchResultsContainer.innerHTML = '';
 
-  // Display each result with content loaded from an external file
-  results.forEach(result => {
-    var resultItem = document.createElement('div');
-    // Display all titles for the result
-    resultItem.innerHTML = `<h3>${result.titles.join(' / ')}</h3><a href="${result.file}">View Details</a>`;
-    searchResultsContainer.appendChild(resultItem);
-  });
+    // Display each result with content loaded from an external file
+    results.forEach(result => {
+        var resultItem = document.createElement('div');
+
+        // Display title
+        resultItem.innerHTML = `<h3>${result.title}</h3>`;
+
+        // Display image if available
+        if (result.img) {
+            var imgElement = document.createElement('img');
+            imgElement.src = result.img;
+            resultItem.appendChild(imgElement);
+        }
+
+        // Display text if available
+        if (result.text) {
+            var textElement = document.createElement('p');
+            textElement.textContent = result.text;
+            resultItem.appendChild(textElement);
+        }
+
+        // Add link to view details
+        var detailsLink = document.createElement('a');
+        detailsLink.href = result.file;
+        detailsLink.textContent = 'View Details';
+        detailsLink.classList.add('details-link'); // Add a class for styling
+        resultItem.appendChild(detailsLink);
+
+        searchResultsContainer.appendChild(resultItem);
+    });
 }
+
+function clearResults() {
+    var searchResultsContainer = document.getElementById('searchResults');
+    // Clear previous results
+    searchResultsContainer.innerHTML = '';
+}
+
 
 //copy text button//
 function copyText(elementId) {
   var preText = document.getElementById(elementId);
   var textToCopy = preText.innerText;
 
-  // Create a temporary textarea element
   var tempTextarea = document.createElement('textarea');
   tempTextarea.value = textToCopy;
   document.body.appendChild(tempTextarea);
 
-  // Select the text in the textarea
   tempTextarea.select();
   tempTextarea.setSelectionRange(0, 99999); // For mobile devices
 
   try {
-    // Attempt to copy the selected text to the clipboard
-    var successful = document.execCommand('copy');
-    var message = successful ? 'Text copied to clipboard' : 'Unable to copy text';
-    console.log(message);
+      var successful = document.execCommand('copy');
+      var message = successful ? 'Text copied to clipboard' : 'Unable to copy text';
+      console.log(message);
   } catch (err) {
-    console.error('Error copying text', err);
+      console.error('Error copying text', err);
   } finally {
-    // Remove the temporary textarea
-    document.body.removeChild(tempTextarea);
+      document.body.removeChild(tempTextarea);
   }
 }
