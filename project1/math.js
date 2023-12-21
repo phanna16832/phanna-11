9176a0b00295a6d2b4bb65c701a8a84fc61bc30f
Phanna16832 <your.email@example.com>
2023-12-21 07:26:37 +0700

    upate

---
+++ project1/math.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var x = Math.pow(2, 0);
+    console.log(x);
+  
+    // round
+    var y = Math.sqrt(12);
+    const xX = Math.round(y);
+    console.log(xX);
+  
+    // ceil
+    var xY = Math.ceil(y);
+    console.log(xY);
+  
+    // function
+    function love() {
+      const m = document.getElementById("text").value;
+      const w = document.getElementById("text1").value;
+      var a = m + " loves " + w;
+      document.getElementById("result").textContent = a;
+    }
+  });
+  
