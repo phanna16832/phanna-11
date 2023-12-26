1bd4191481d4a747a2a548242341ff2cb14bbb95
Phanna16832 <your.email@example.com>
2023-12-26 07:29:41 +0700

    update

--- project1/math.js
+++ project1/math.js
@@ -1,13 +1,25 @@
-var input = document.getElementById("age").valuAsNumber;
+  // JavaScript code
+  function submitUsername() {
+    let username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = "Hello " + username;
+}
+function love() {
+var x = document.getElementById("text").value;
+var y = document.getElementById("text1").value;
+document.getElementById("result").textContent = x + " loves " + y;
+}
+function checkAge() {
+var ageInput = document.getElementById("age").valueAsNumber;
 var output = document.getElementById("age2");
-function age(){
-  if(input <=99){
-    output.textContent = "jas hv rok ss tt";
-  }
-  else if (input >=18){
-    output.textContent = "single dol na";
-  }
-  else if (input <18){
+
+if (isNaN(ageInput) || ageInput < 0) {
+    output.textContent = "Please enter a valid age.";
+} else if (ageInput <= 18) {
     output.textContent = "kmeng jg kom ton al mean ss";
-  }
+} else if (ageInput <= 45) {
+    output.textContent = "kal yok ss";
+}
+else{
+  output.textContent = ageInput <= 60 ? "jas hv rok ss tt" : "";
+}
 }
