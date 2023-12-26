235e4d5d4970b063011641018f38327ab860844d
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2023-12-26 22:40:27 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,68 +1,55 @@
-//search bar
 document.addEventListener("DOMContentLoaded", function () {
-    var searchButton = document.getElementById('searchButton');
-    searchButton.addEventListener('click', performSearch);
+    var searchForm = document.getElementById('searchForm');
+    searchForm.addEventListener('submit', performSearch);
 });
 
-function performSearch() {
-    // Get the input value from the search bar
-    var searchInput = document.getElementById('searchBar').value.toLowerCase();
+function performSearch(event) {
+    event.preventDefault();
 
-    // Check if the search input is not empty
-    if (searchInput.trim() !== '') {
-        // Perform your search logic here
+    var searchInputValue = document.getElementById('searchInput').value.toLowerCase();
+
+    if (searchInputValue.trim() !== '') {
         var data = [
             { title: "Telegram", file: "telegramSave reply.html" },
             { title: "CCSL", file: "ccsl.html"},
             { title: "Fee Calculation", file: "calculate.html"},
             { title: "Taobao and Pinduoduo", file: "project1.html", img: "taobao1.jpg"},
             { title: "id and CE ID", text: "Password:888888 ID:  លេខទូរសព្ទ័: 0 ", file: "#" }
-            // { title: "id and CE ID", file: " ", text: "Password:888888 ID:  លេខទូរសព្ទ័: 0 " }
             // Add more results as needed
         ];
 
-        // Filter data based on the search input
-        var filteredResults = data.filter(result => result.title.toLowerCase().includes(searchInput));
+        var filteredResults = data.filter(result => result.title.toLowerCase().includes(searchInputValue));
 
-        // Display the results in the searchResults div
         displayResults(filteredResults);
     } else {
-        // Clear the search results if the input is empty
         clearResults();
     }
 }
 
 function displayResults(results) {
     var searchResultsContainer = document.getElementById('searchResults');
-    // Clear previous results
     searchResultsContainer.innerHTML = '';
 
-    // Display each result with content loaded from an external file
     results.forEach(result => {
         var resultItem = document.createElement('div');
-
-        // Display title
         resultItem.innerHTML = `<h3>${result.title}</h3>`;
 
-        // Display image if available
         if (result.img) {
             var imgElement = document.createElement('img');
             imgElement.src = result.img;
             resultItem.appendChild(imgElement);
         }
 
-        // Display text if available
         if (result.text) {
             var textElement = document.createElement('p');
             textElement.textContent = result.text;
             resultItem.appendChild(textElement);
         }
 
-        // Add link to view details
         var detailsLink = document.createElement('a');
         detailsLink.href = result.file;
         detailsLink.textContent = 'View Details';
-        detailsLink.classList.add('details-link'); // Add a class for styling
+        detailsLink.classList.add('details-link');
         resultItem.appendChild(detailsLink);
 
         searchResultsContainer.appendChild(resultItem);
@@ -71,7 +58,6 @@ function displayResults(results) {
 
 function clearResults() {
     var searchResultsContainer = document.getElementById('searchResults');
-    // Clear previous results
     searchResultsContainer.innerHTML = '';
 }
 
